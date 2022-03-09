ab523133ac38e22e1ecf13ce6c0156cd52880a9d
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-09 15:49:05 +0100

    referencie objektov a kopia pola

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,12 +1,20 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.kontakty.Kontakt;
+
 public class Main {
     public static void main(String[] args) {
-        Test test =new Test();
-        test.tri =4;
+        var tomas = new Kontakt("Tomas","Kovacik");
+        var jakub = new Kontakt("Jakub Kovar");
+        var dominik = new Kontakt("Dominik Pruzisnky");
 
-        do{
+        var prednasajuci = new Kontakt("Gabriel","Juhas");
 
-        } while (test.tri == 3);
+        var kontakty = new Kontakt[]{new Kontakt(tomas), new Kontakt(jakub),new Kontakt(dominik)};
+        prednasajuci.pridajZnamych(kontakty);
+
+        prednasajuci.vypisZnamych();
+        tomas.setMeno("Tomaskooo");
+        prednasajuci.vypisZnamych();
     }
 }
